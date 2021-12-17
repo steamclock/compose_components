163db83a5113131c8ec8b6011dc300f69a70b173
Jacob Miner <jake@steamclock.com>
2021-12-17 12:20:11 -0800

    Add ThemedDivider component

---
+++ Sample/components/src/main/java/com/steamclock/components/components/ThemedDivider.kt
@@ -0,0 +1,46 @@
+package com.steamclock.components.components
+
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.steamclock.components.theme.NiceComponentsTheme
+
+/**
+ * Sample
+ * Created by jake on 2021-12-17, 12:16 p.m.
+ */
+@Composable
+fun ThemedDivider(
+    modifier: Modifier = Modifier
+) {
+    Divider(
+        modifier = modifier.alpha(0.6f),
+        color = MaterialTheme.colors.onPrimary,
+        thickness = 1.dp
+    )
+}
+
+@Preview
+@Composable
+fun ScreenTitlePreview() {
+    NiceComponentsTheme {
+        Surface {
+            ThemedDivider()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScreenTitlePreviewDark() {
+    NiceComponentsTheme(darkTheme = true) {
+        Surface {
+            ThemedDivider()
+        }
+    }
+}
