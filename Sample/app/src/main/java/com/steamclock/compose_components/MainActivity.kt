8524f53d12d38132d2a42d4ae753387ce954146d
Jacob Miner <jake@steamclock.com>
2021-12-17 14:18:21 -0800

    Setup navigation to screens

--- Sample/app/src/main/java/com/steamclock/compose_components/MainActivity.kt
+++ Sample/app/src/main/java/com/steamclock/compose_components/MainActivity.kt
@@ -3,36 +3,70 @@ package com.steamclock.compose_components
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.steamclock.compose_components.ui.theme.SampleTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.steamclock.components.theme.NiceComponentsTheme
+import com.steamclock.compose_components.views.AllComponents
+import com.steamclock.compose_components.views.Home
+import com.steamclock.compose_components.views.SignIn
+import com.steamclock.compose_components.views.StatefulExample
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContent { Content() }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun Content() {
+    val navController = rememberNavController()
+    Scaffold(topBar = {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        val currentRoute = currentDestination?.route ?: ""
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SampleTheme {
-        Greeting("Android")
+        TopAppBar(
+            title = {
+                Text(text = currentRoute)
+            },
+            navigationIcon = {
+                if (currentRoute != Routes.Home) {
+                    Image(imageVector = Icons.Filled.ArrowBack,
+                        colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary),
+                        contentDescription = "Back",
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .clickable {
+                                navController.popBackStack()
+                            }
+                    )
+                }
+            }
+        )
+    }) {
+        NiceComponentsTheme {
+            Surface {
+                NavHost(navController = navController, startDestination = Routes.Home) {
+                    composable(Routes.Home) { Home(navController) }
+                    composable(Routes.AllComponents) { AllComponents() }
+                    composable(Routes.SignIn) { SignIn() }
+                    composable(Routes.StatefulView) { StatefulExample() }
+                }
+            }
+        }
     }
 }
