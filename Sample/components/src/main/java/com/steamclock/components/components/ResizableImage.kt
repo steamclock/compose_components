c6a61fe321f1fd72248e66bd471fe8a0cbf93c93
Jacob Miner <jake@steamclock.com>
2021-12-17 12:34:12 -0800

    Add ResizableImage component

---
+++ Sample/components/src/main/java/com/steamclock/components/components/ResizableImage.kt
@@ -0,0 +1,47 @@
+package com.steamclock.components.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.rememberImagePainter
+import com.steamclock.components.theme.NiceComponentsTheme
+
+/**
+ * Sample
+ * Created by jake on 2021-12-17, 12:31 p.m.
+ */
+@Composable
+fun ResizableImage(
+    url: String,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = rememberImagePainter(url),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun ResizableImagePreview() {
+    NiceComponentsTheme {
+        Surface {
+            // note: displaying images from a URL doesn't work in preview
+            ResizableImage("https://www.example.com/image.jpg")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ResizableImagePreviewDark() {
+    NiceComponentsTheme(darkTheme = true) {
+        Surface {
+            // note: displaying images from a URL doesn't work in preview
+            ResizableImage("https://www.example.com/image.jpg")
+        }
+    }
+}
