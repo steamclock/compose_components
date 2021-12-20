bc474b1517db810ffd50bc6cded6dd1608e2731a
Jacob Miner <jake@steamclock.com>
2021-12-20 11:26:41 -0800

    Remove ColorExtensions

--- Sample/components/src/main/java/com/steamclock/components/theme/NiceComponentsTheme.kt
+++ Sample/components/src/main/java/com/steamclock/components/theme/NiceComponentsTheme.kt
@@ -4,40 +4,39 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.steamclock.components.extensions.fromHex
 
 /**
  * Sample
  * Created by jake on 2021-12-17, 10:48 a.m.
  */
 val NiceComponentsLightColors = lightColors(
-    primary = Color.fromHex("#4F46E5"),
-    onPrimary = Color.fromHex("#F9FAFB"),
-    primaryVariant = Color.fromHex("#4338CA"),
-    secondary = Color.fromHex("#059669"),
-    onSecondary = Color.fromHex("#F9FAFB"),
-    secondaryVariant = Color.fromHex("#047857"),
-    error = Color.fromHex("#DC2626"),
-    onError = Color.fromHex("#F9FAFB"),
-    onSurface = Color.fromHex("#111827"),
-    surface = Color.fromHex("#F3F4F6"),
-    background = Color.fromHex("#E5E7EB"),
-    onBackground = Color.fromHex("#111827")
+    primary =  Color(0xFF4F46E5),
+    onPrimary = Color(0xFFF9FAFB),
+    primaryVariant = Color(0xFF4338CA),
+    secondary = Color(0xFF059669),
+    onSecondary = Color(0xFFF9FAFB),
+    secondaryVariant = Color(0xFF047857),
+    error = Color(0xFFDC2626),
+    onError = Color(0xFFF9FAFB),
+    onSurface = Color(0xFF111827),
+    surface = Color(0xFFF3F4F6),
+    background = Color(0xFFE5E7EB),
+    onBackground = Color(0xFF111827)
 )
 
 val NiceComponentsDarkColors = darkColors(
-    primary = Color.fromHex("#4F46E5"),
-    onPrimary = Color.fromHex("#F9FAFB"),
-    primaryVariant = Color.fromHex("#4338CA"),
-    secondary = Color.fromHex("#059669"),
-    onSecondary = Color.fromHex("#F9FAFB"),
-    secondaryVariant = Color.fromHex("#047857"),
-    error = Color.fromHex("#DC2626"),
-    onError = Color.fromHex("#F9FAFB"),
-    onSurface = Color.fromHex("#F9FAFB"),
-    surface = Color.fromHex("#1F2937"),
-    background = Color.fromHex("#111827"),
-    onBackground = Color.fromHex("#E5E7EB")
+    primary =  Color(0xFF4F46E5),
+    onPrimary = Color(0xFFF9FAFB),
+    primaryVariant = Color(0xFF4338CA),
+    secondary = Color(0xFF059669),
+    onSecondary = Color(0xFFF9FAFB),
+    secondaryVariant = Color(0xFF047857),
+    error = Color(0xFFDC2626),
+    onError = Color(0xFFF9FAFB),
+    onSurface = Color(0xFFF9FAFB),
+    surface = Color(0xFF1F2937),
+    background = Color(0xFF111827),
+    onBackground = Color(0xFFE5E7EB)
 )
 
 @Composable
